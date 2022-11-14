fb38544cef2a6591f0be62fc554659126fdc0b00
Yzabakli <abakliyusufziya@gmail.com>
2022-11-14 22:46:00 +0100

    Initial commit

---
+++ src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -0,0 +1,55 @@
+package com.cydeo.entity.common;
+
+import com.cydeo.entity.User;
+import com.cydeo.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Set.of(new SimpleGrantedAuthority(user.getRole().getDescription()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassWord();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
